d8a28cdf69874116ac577addaa9733bdc9c99c5b
vuphuong1794 <phuongvu1794@gmail.com>
2024-12-09 11:40:31 +0700

    binding event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   standalone: true,
 })
 export class AppComponent {
   items: WishItem[] = [
-    // new WishItem('To learn Angular', true),
-    // new WishItem('To learn TypeScript'),
+    new WishItem('To learn Angular', true),
+    new WishItem('To learn TypeScript'),
+    new WishItem('To become a software Engineer'),
   ];
   title = 'WishList';
+
+  toggle(item: WishItem) {
+    item.isComplete = !item.isComplete;
+    console.log(item);
+  }
 }

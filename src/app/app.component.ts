6f545b6d6e6f2379248ceefe3a90425e78cb5c31
vuphuong1794 <phuongvu1794@gmail.com>
2024-12-09 11:16:58 +0700

    making decisions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true,
 })
 export class AppComponent {
-  title = 'firtAngular';
+  items: WishItem[] = [
+    // new WishItem('To learn Angular', true),
+    // new WishItem('To learn TypeScript'),
+  ];
+  title = 'WishList';
 }
